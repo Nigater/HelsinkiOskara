1973bdd23b3f285cc8953ac62a8eb334c52bb86b
oskar <oskarpawlowiec11864@wp.pl>
2023-10-19 11:50:55 +0200

    sdada

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,34 +1,47 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import { render } from 'react-dom'
 
-function App() {
-  const [count, setCount] = useState(0)
+const part1 = 'Fundamentals of React'
+const exercises1 = 10
+const part2 = 'Using props to pass data'
+const exercises2 = 7
+const part3 = 'State of a component'
+const exercises3 = 14
+const course = 'Half Stack application development'
 
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <Header course={course} />
+      <Content />
+      
+    </div>
+  )
+}
+
+const Header = ({course}) => {
+  return (
+    <h1>{course}</h1>
+  )
+}
+
+const Content = () => {
+  return (
+    <div>
+      <Part part={part1} exercises={exercises1} />
+      <Part part={part2} exercises={exercises2} />
+      <Part part={part3} exercises={exercises3} />
+    </div>
+  )
+}
+
+const Part = ({part, exercises}) => {
+  return (
+    <p>
+      {part} {exercises}
+    </p>
   )
 }
 
